0ad9a54c6c29fd4116cc235bd15c5a03d15cb6b6
Joede Nasciemento <joedenascimento2@gmail.com>
2019-06-09 11:34:24 -0300

    mensagens de erro

--- src/app/cursos/adicionar-curso/adicionar-curso.component.ts
+++ src/app/cursos/adicionar-curso/adicionar-curso.component.ts
@@ -45,4 +45,8 @@ export class AdicionarCursoComponent implements OnInit {
     );
   }
 
+  mostraErro(campo){
+    return campo.invalid && campo.touched;
+  }
+
 }
--- src/app/cursos/editar-curso/editar-curso.component.ts
+++ src/app/cursos/editar-curso/editar-curso.component.ts
@@ -64,4 +64,8 @@ export class EditarCursoComponent implements OnInit {
     );
   }
 
+  mostraErro(campo){
+    return campo.invalid && campo.touched;
+  }
+
 }
